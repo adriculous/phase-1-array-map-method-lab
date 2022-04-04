2b47dfd6dcc3c81eaa72111f94cda8925ecea8bf
adriculous <admarpad@gmail.com>
2022-04-03 17:01:16 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const tutTitles = tutorials.map((wordsInTitles) => {
+    const tutTitlesList = wordsInTitles
+      .split(" ")
+      .map((wordies) => {
+        return wordies.charAt(0).toUpperCase() + wordies.slice(1);
+      })
+      .join(" ");
+    return tutTitlesList;
+  });
+  return tutTitles;
+};
+
+// freeCodeCamp: How to Capitalize Words in JavaScript? (https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/)
+
+// charAt() (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt)
